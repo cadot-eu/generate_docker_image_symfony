9fc34edfd0f5f2b85a07e8dc963ca053640d4185
michael <michael@cadot.eu>
2025-03-28 10:30:43 +0100

    add wwww-data sur public

--- BuildConfig/docker-entrypoint.sh
+++ BuildConfig/docker-entrypoint.sh
@@ -19,6 +19,8 @@ if [ ! -f bin/console ]; then
     
 fi
 
+git config --global --add safe.directory /app
+
 # gestion répertoire var
 if [ ! -d var ]; then
     log_warn "Création du répertoire var..."
@@ -30,23 +32,23 @@ fi
 
 
 # Installation des dépendances si nécessaire
-if [ ! -d vendor ]; then
-    log_warn "Installation des dépendances via Composer..."
-    if composer install --prefer-dist --no-progress --no-interaction; then
-        log_success "Dépendances installées avec succès"
-    else
-        log_error "Échec de l'installation des dépendances"
-        
-    fi
+log_warn "Installation des dépendances via Composer..."
+if composer install --prefer-dist --no-progress --no-interaction; then
+    log_success "Dépendances installées avec succès"
+else
+    log_error "Échec de l'installation des dépendances"
+    
 fi
 
 
 # importmap install
-log_warn "Installation de importmap..."
-php bin/console importmap:install --no-interaction || {
-    log_error "Échec de l'installation de importmap"
-    
-}
+if [ ! -f public/vendor/importmap ]; then
+    log_warn "Installation de importmap..."
+    php bin/console importmap:install --no-interaction || {
+        log_error "Échec de l'installation de importmap"
+        
+    }
+fi
 
 # Mise à jour de la base de données
 log_warn "Mise à jour de la base de données..."
@@ -72,25 +74,25 @@ fi
 # Compilation des assets si la commande existe
 
 log_warn "Compilation des assets..."
-if [ "$MODE" = "prod" ]; then
-    if php bin/console | grep -q asset-map:compile; then
-        php bin/console asset-map:compile --no-interaction || {
-            log_error "Erreur durant asset-map:compile"
-            
-        }
-    else
-        log_warn "Commande asset-map:compile non disponible - ignorée"
-    fi
+if php bin/console | grep -q asset-map:compile; then
+    php bin/console asset-map:compile --no-interaction || {
+        log_error "Erreur durant asset-map:compile"
+        
+    }
+else
+    log_warn "Commande asset-map:compile non disponible - ignorée"
 fi
 
 #installion de php flasher
 log_warn "Installation de php flasher..."
-if [ ! -f public/vendor/flasher ]; then
+if [ ! -f public/vendor/flasher ] && [ -d vendor/php-flasher ]; then
     log_warn "Installation de php flasher..."
     php bin/console flasher:install
 fi
 
-
+#permission www-data sur public
+log_warn "Permission www-data sur public..."
+chown -R www-data:www-data public
 
 
 # Création du raccourci sc pour Symfony Console
